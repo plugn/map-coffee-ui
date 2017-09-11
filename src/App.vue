cdee29dc21f3ba0ae4c403ccbf4026250acb029a
Maxim Dolgov <bananafishbone@gmail.com>
2017-09-11 20:27:27 +0300

    optimize css

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 	<div id="app" class="flex-col">
 
 		<header class="keyline-bottom keyline-lighten0 title3d">map.coffee</header>
-		<div class="stretch" style="position: relative;">
+		<div class="stretch fill-canvas" style="position: relative;">
 			<MapGL></MapGL>
 		</div>
 
@@ -36,16 +36,11 @@
 	}
 
 	.keyline-bottom {
-		border-bottom-width: 1px;
-		border-bottom-style: solid;
-		border-bottom-color: rgba(0, 0, 0, 0.1);
+		border-bottom: 1px solid rgba(0, 0, 0, .1);
 	}
 
 	.dark .keyline-lighten0, .keyline-lighten0 {
-		border-top-color: rgba(255, 255, 255, 0.1);
-		border-right-color: rgba(255, 255, 255, 0.1);
-		border-bottom-color: rgba(255, 255, 255, 0.1);
-		border-left-color: rgba(255, 255, 255, 0.1);
+		border-color: rgba(255, 255, 255, .1)
 	}
 
 	header {
@@ -53,15 +48,12 @@
 		line-height: 2rem;
 		text-align: right;
 		background-color: rgb(89, 91, 91);
-		color: rgb(255, 255, 255);
+		color: #fff;
 		min-height: 50px;
 	}
 
 	.title3d {
-		padding-top: 0px;
-		padding-right: 1.25rem;
-		padding-bottom: 0px;
-		padding-left: 1.25rem;
+		padding: 0 1.25rem;
 		height: 3rem;
 		line-height: 3rem;
 		letter-spacing: 1px;
